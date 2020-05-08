a054168ece8ee4673eddccb49f8a089d0e675401
bowtieman143 <steven@onloaddevelopment.com>
2020-05-08 08:56:24 -0700

    Removed dashboard reference from portal router

--- src/pages/portal.js
+++ src/pages/portal.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Router } from "@reach/router"
 import Profile from "../private-pages/Profile"
-import Dashboard from "../private-pages/Dashboard"
 import Login from "../private-pages/Login"
 import PrivateRoute from "../components/PrivateRoute"
 
 const Portal = () => {
   return (
     <Router basepath="/portal">
-      <PrivateRoute path="/" component={Dashboard} />
       <PrivateRoute path="/profile" component={Profile} />
       <Login path="/login" />
     </Router>
