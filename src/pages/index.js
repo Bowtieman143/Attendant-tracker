a5f17528c2b264a12d0b7477b63b6a019443a4c5
bowtieman143 <steven@onloaddevelopment.com>
2020-05-08 08:06:53 -0700

    Fixed casing issues from all component references

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import LoginForm from "../components/LoginForm"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
--- src/private-pages/Login.js
+++ src/private-pages/Login.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Layout from "../components/layout"
-import LoginForm from "../components/loginForm"
+import Layout from "../components/Layout"
+import LoginForm from "../components/LoginForm"
 
 const Login = () => {
   return (
